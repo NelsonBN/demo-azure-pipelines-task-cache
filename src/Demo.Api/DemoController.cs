e2086a681a49cfd219177b6e30eaabbd795dc137
Nelson Nobre <technobre@gmail.com>
2023-02-18 02:23:48 +0000

    test: pipeline

--- src/Demo.Api/DemoController.cs
+++ src/Demo.Api/DemoController.cs
@@ -1,4 +1,4 @@
-using Demo.Lib;
+﻿using Demo.Lib;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.Api;
@@ -8,14 +8,8 @@ namespace Demo.Api;
 public class DemoController : ControllerBase
 {
     [HttpGet("country")]
-    public IActionResult GetCountry([FromQuery] string iso2)
-    {
-        return Ok(Utils.GetCountry(iso2));
-    }
+    public IActionResult GetCountry([FromQuery] string iso2) => Ok(Utils.GetCountry(iso2));
 
     [HttpGet("compress-text")]
-    public IActionResult CompressText([FromQuery] string name)
-    {
-        return Ok(PowerUtils.Text.PowerUtilsExtensions.CompressText(name, 5));
-    }
+    public IActionResult CompressText([FromQuery] string name) => Ok(PowerUtils.Text.PowerUtilsExtensions.CompressText(name, 4));
 }
